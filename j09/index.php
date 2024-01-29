bbf0e7ec5579092c5ffeaebb2e1e24b9345670b9
reza nazeri <reza.armani75@gmail.com>
2024-01-29 14:51:53 +0330

    update j9

--- j09/index.php
+++ j09/index.php
@@ -6,30 +6,47 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 
+  <style>
+    .title {
+      color: red;
+    }
+
+    .image {
+      width: 300px;
+    }
+  </style>
 </head>
 
 <body>
   <?php
-  $myList = [
+  $myGame = [
     [
-      "title" => "The last of us",
-      "description" => "lorem lorem lorem",
-      "rank" => 8.4,
+      "title" => "GTA V",
+      "rank" => 7.8,
+      "description" => "Best Open world",
+      "image" => "./image/GTA-V.webp",
     ],
     [
       "title" => "Horizon",
-      "description" => "lorem lorem lorem",
-      "rank" => 9.4,
+      "rank" => 8.7,
+      "description" => "Best Nustalghi game",
+      "image" => "./image/horizon.jpeg",
+    ],
+    [
+      "title" => "The last of us",
+      "rank" => 9.0,
+      "description" => "Best local multiplayer",
+      "image" => "./image/the-last-of-us-1.jpg",
     ],
   ];
 
-  foreach ($myList as $item) {
+  foreach ($myGame as $item) {
     echo "
-    <dev>
-      <h1>{$item['title']}</h1>
-      <p>{$item['description']}</p>
-      <div>Rank: {$item['rank']} / 10</div>
-    </dev>";
+    <h2 class='title'>{$item['title']}</h2>
+    <img class='image' src='{$item['image']}'></img>
+    <p>{$item['description']}</p>
+    <div>{$item['rank']}</div>
+    ";
   }
 
   ?>
