d6b489e0018f8a833637951ba80b261bd244c089
Reza Nazeri <reza.armani75@gmail.com>
2024-05-06 15:47:59 +0330

    add j12

---
+++ j12/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <form method="POST">
+    <h2>Create</h2>
+    <div>
+      <input type="text" name="name" placeholder="نام" required>
+    </div>
+
+    <div>
+      <input type="number" name="paye_tahsili" placeholder="پایه تحصیلی" required>
+    </div>
+
+    <div>
+      <input type="date" name="tarikh_tavalod" placeholder="تاریخ تولد" required>
+    </div>
+
+    <div>
+      <button type="submit" name="submit">ثبت</button>
+    </div>
+  </form>
+</body>
+
+</html>
+
+<?php
+//                 server address, username,password,db name
+$con = mysqli_connect("localhost", "root", "root", "school");
+
+if (mysqli_connect_errno()) {
+  echo "Connection Fail" . mysqli_connect_error();
+} else {
+  echo "<p>Connected</p>";
+}
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $paye_tahsili = $_POST['paye_tahsili'];
+  $tarikh_tavalod = $_POST['tarikh_tavalod'];
+
+  $query = mysqli_query($con, "INSERT INTO student ( name, paye_tahsili, tarikh_tavalod ) VALUES ('$name','$paye_tahsili', '$tarikh_tavalod' )");
+
+  if ($query) {
+    echo "<p>Success</p>";
+  } else {
+    echo "error on insert";
+  }
+}
+?>
